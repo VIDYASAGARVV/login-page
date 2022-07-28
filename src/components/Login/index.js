3aafa26eb2bc7a8d12c8fd0417bb549359b7ea14
sagar <vvsagar30@gmail.com>
2022-07-28 04:49:20 +0000

    login -page

---
+++ src/components/Login/index.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+// Write your code here
+import './index.css'
+
+const Login = props => {
+  const {login} = props
+
+  return (
+    <button type="button" className="login-button" onClick={login}>
+      Login
+    </button>
+  )
+}
+
+export default Login
